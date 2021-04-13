103051b8fc284d5876446db71c4faa9e2a577fe4
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-04-13 19:05:53 +0800

    Update db_conn.php

--- php/db_conn.php
+++ php/db_conn.php
@@ -5,16 +5,25 @@
 // $dbname = "id16218880_buceils";
 
 // Den
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "bucielsmain2";    
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "bucielsmain2";    
     
-    // Create connection
-    $conn = mysqli_connect($servername,$username,$password,$dbname);
-    if (!$conn) {
-        printf("Connect failed: %s\n", mysqli_connect_error());
-        exit();
+//     // Create connection
+//     $conn = mysqli_connect($servername,$username,$password,$dbname);
+//     if (!$conn) {
+//         printf("Connect failed: %s\n", mysqli_connect_error());
+//         exit();
+//     }
+
+// JP
+    $dbhost = "localhost";
+    $dbuser = "id16218880_webhostingbscs3a";
+    $dbpass = "t9%~bjqmK)uHAwe[";
+    $db = "id16218880_buceils";
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
+    if ($conn->connect_error) {
+        die("Connection to database failed: ". $conn->connect_error);
     }
- 
 ?>
