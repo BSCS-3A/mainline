af64beacaa1d76daa2be89c23c5df6eb0a80ccf6
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-04-25 21:26:38 +0800

    New submission algorithm

--- php/vtSubmit.php
+++ php/vtSubmit.php
@@ -1,56 +1,74 @@
 <?php
 	$table = $conn->query("SELECT * FROM ((candidate INNER JOIN student ON candidate.student_id = student.student_id) INNER JOIN candidate_position ON candidate.position_id = candidate_position.position_id) ORDER BY candidate_position.heirarchy_id"); // get positions
+	
+	$choice_final = array();
+	$vote_table = array();
 
+	// Sanitation and Validation
 	mysqli_data_seek($table, 0);
 	$stud_id = $_SESSION['student_id'];
 	$status = "";
 	while($poss = $table->fetch_assoc()){   // loop through all positions
 		if(($poss["vote_allow"] == 0 && $_SESSION['grade_level'] == $poss["grade_level"]) || $poss["vote_allow"] == 1){
 			if(empty(($_POST[$poss['heirarchy_id']]))){
-				// insert sanitation and validation function here
 				$choice = 0;
 			}
 			else{
-				$_POST[$poss['heirarchy_id']] = filter_var($_POST[$poss['heirarchy_id']], FILTER_SANITIZE_STRING);
-				// insert sanitation and validation function here
-				$choice = cleanInput($_POST[$poss['heirarchy_id']]);
-				// $choice = $_POST[$poss['heirarchy_id']];
+				$_POST[$poss['heirarchy_id']] = filter_var($_POST[$poss['heirarchy_id']], FILTER_SANITIZE_STRING);;
+				$choice = $_POST[$poss['heirarchy_id']];
 			}
-			
-			// if(isValidCandidate($table, $choice, $poss['heirarchy_id'])){
+
+			if(isValidCandidate($conn, $choice, $poss['heirarchy_id'])){
 				if($poss['candidate_id'] == $choice){
-					$candidate = $poss['candidate_id'];
-					$conn->query("UPDATE candidate SET total_votes = total_votes + 1 WHERE candidate.candidate_id = $candidate");		
-					// echo "Voted <br>";
+					$choice_final[$poss['heirarchy_id']] = $poss['candidate_id'];
 					$status = "Voted";
 				}
 				else{
-					// echo "Abstain <br>";
 					$status = "Abstain";
 				}
-			// }
-			// else{
-			// 	// notify admin & return to ballot
-			// 	echo "Ballot tampering detected.";
-			// 	exit();
-			// 	// header("Location: vtBallot.php");
-			// }
+			}
+			else{
+				// notify admin & return to ballot
+				// echo "Ballot tampering detected.";
+				errorMessage("Ballot tampering detected. You have been banned from voting and reported to the admins.");
+				mysqli_data_seek($table, 0);
+				while($poss = $table->fetch_assoc()){
+					$cand_id = $poss['candidate_id'];
+					$conn->query("INSERT INTO `vote` (`vote_log_id`, `student_id`, `candidate_id`, `status`, `time_stamp`) VALUES (NULL, $stud_id, $cand_id, 'Invalid', current_timestamp())");
+				}
+				exit();
+				// header("Location: index.php");
+			}
 		}
 		else{
-			$status = "Abstain";
+			$status = " Abstain";
 		}
+		$vote_table[$poss['candidate_id']] = $status;
+	}
 
-		
-		// // send vote table
-		// $cand_id = $poss['student_id'];
+	// Submission
+	mysqli_data_seek($table, 0);
+	while($poss = $table->fetch_assoc()){
+		if(($poss["vote_allow"] == 0 && $_SESSION['grade_level'] == $poss["grade_level"]) || $poss["vote_allow"] == 1){
+			$choice = $choice_final[$poss['heirarchy_id']];
+			if($poss['candidate_id'] == $choice){
+				$candidate = $poss['candidate_id'];
+				$conn->query("UPDATE candidate SET total_votes = total_votes + 1 WHERE candidate.candidate_id = $candidate");	
+			}
+		}
+	}
+	
+	mysqli_data_seek($table, 0);
+	while($poss = $table->fetch_assoc()){
+		$status = $vote_table[$poss['candidate_id']];
 		$cand_id = $poss['candidate_id'];
 		$conn->query("INSERT INTO `vote` (`vote_log_id`, `student_id`, `candidate_id`, `status`, `time_stamp`) VALUES (NULL, $stud_id, $cand_id, '$status', current_timestamp())");
 	}
+	
 
 	// update voter's status
 	$conn->query("UPDATE `student` SET `voting_status` = true WHERE `student`.`student_id` = '$stud_id'");
-	// generate reciept
-	// show receipt
+
 
 
 
