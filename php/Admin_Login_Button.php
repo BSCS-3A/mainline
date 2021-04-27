77d70c5d40bdb1070836a6ef0ca880ccfb4619b2
JoshuaAlexLlander <79438597+JoshuaAlexLlander@users.noreply.github.com>
2021-04-27 21:44:00 +0800

    added validation for illegal characters
    also reconstructed conditions for login and login w/ captcha

--- php/Admin_Login_Button.php
+++ php/Admin_Login_Button.php
@@ -2,8 +2,12 @@
 session_start();
 include ('db_conn.php');
 
+
 if (isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
-    if(isset($_POST['g-recaptcha-response'])) {
+    
+if($_SESSION['incorrectTry']>=3){
+    //login with captcha
+ if(isset($_POST['g-recaptcha-response'])) {
    // RECAPTCHA SETTINGS
    $captcha = $_POST['g-recaptcha-response'];
    $ip = $_SERVER['REMOTE_ADDR'];
@@ -21,20 +25,25 @@ if (isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
      header("Location: AdminLogin.php?error=Oops! It seems like you logged in incorrectly for 3 times. Please verify that you are not a bot by clicking the checkbox before logging in..");	
      exit();
    }
- }
-
-	function validate($data){
-       $data = trim($data);
-	   $data = stripslashes($data);
-	   $data = htmlspecialchars($data);
-	   return $data;
-	}
-//get  data from the textbox
-	$username = validate($_REQUEST['username']);
-	$pass = validate($_REQUEST['password']); //hash algorithm here
+ }else {
+     // die('Your account has been logged as a spammer, you cannot continue!');
+     header("Location: AdminLogin.php?error=Oops! It seems like you logged in incorrectly for 3 times. Please verify that you are not a bot by clicking the checkbox before logging in..");	
+     exit();
+   }
+   
+	function validate($conn, $data){
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		$data = $conn->real_escape_string($data);	//added $conn->real_escape_string: escapes special characters 26/04/2021
+		return $data;
+	 }
+ //gets the input from the textbox in the StudentLogin.php
+	 $username = validate($conn, $_POST['username']);
+	 $pass = validate($conn, $_POST['password']); //hash here
 
 	if (empty($username)) {
-		header("Location: AdminLogin.php?error=User Name is required");
+		header("Location: AdminLogin.php?error=Email is required");
 	    exit();
 	}else if(empty($pass)){
         header("Location: AdminLogin.php?error=Password is required");
@@ -52,7 +61,7 @@ if (isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
 				$_SESSION['admin_lname'] = $row['admin_lname'];
             	$_SESSION['admin_id'] = $row['admin_id'];
             	$_SESSION['photo'] = $row['photo'];
-		$_SESSION['admin_position'] = $row['admin_position'];  	//added for enabling/hiding some features for admin (04/04/2021, 2:36pm)
+				$_SESSION['admin_position'] = $row['admin_position'];  	//added for enabling/hiding some features for admin (04/04/2021, 2:36pm)
             	$_SESSION['timestamp']=time(); //added for time session
 				$admin_id = $row['admin_id'];
 				date_default_timezone_set('Asia/Manila');
@@ -72,10 +81,68 @@ if (isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
 			$_SESSION['incorrectTry']++;
 	        exit();
 		}
-	}
+	    }
+}
+else{
+	function validate($conn, $data){
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		$data = $conn->real_escape_string($data);	//added $conn->real_escape_string: escapes special characters 26/04/2021
+		return $data;
+	 }
+ //gets the input from the textbox in the StudentLogin.php
+	 $username = validate($conn, $_POST['username']);
+	 $pass = validate($conn, $_POST['password']); //hash here
 
+	if (empty($username)) {
+		header("Location: AdminLogin.php?error=Email is required");
+	    exit();
+	}else if(empty($pass)){
+        header("Location: AdminLogin.php?error=Password is required");
+	    exit();
+	}else{
+		$sql = "SELECT * FROM admin WHERE username='$username' AND password='$pass'";
+
+		$result = mysqli_query($conn, $sql);
+//check if the inputs are correct if yes then go to the dashboard
+		if (mysqli_num_rows($result)===1) {
+			$row = mysqli_fetch_assoc($result);
+            if ($row['username'] === $username && $row['password'] === $pass) {
+            	$_SESSION['username'] = $row['username'];
+            	$_SESSION['admin_fname'] = $row['admin_fname'];
+				$_SESSION['admin_lname'] = $row['admin_lname'];
+            	$_SESSION['admin_id'] = $row['admin_id'];
+            	$_SESSION['photo'] = $row['photo'];
+				$_SESSION['admin_position'] = $row['admin_position'];  	//added for enabling/hiding some features for admin (04/04/2021, 2:36pm)
+            	$_SESSION['timestamp']=time(); //added for time session
+				$admin_id = $row['admin_id'];
+				date_default_timezone_set('Asia/Manila');
+				$date = date('Y-m-d');
+				$time = date('H:i:s');
+				mysqli_query($conn, "INSERT INTO admin_activity_log(admin_id,activity_description,activity_date,activity_time) VALUES('$admin_id','Login','$date','$time')");
+			    
+            	header("Location: AdminDashboard.php");
+		        exit();
+            }else{
+				header("Location: AdminLogin.php?error=Incorrect Username or Password");
+				 $_SESSION['incorrectTry']++;
+		        exit();
+			}
+		}else{
+			header("Location: AdminLogin.php?error=Incorrect Username or Password");
+			$_SESSION['incorrectTry']++;
+	        exit();
+		}
+	    }
+    
+}
+    
+ 
 }else{
 	header("Location: ..\Login UI v2\html\AdminLogin.php");
 	$_SESSION['incorrectTry']++;
 	exit();
 }
+
+?>
