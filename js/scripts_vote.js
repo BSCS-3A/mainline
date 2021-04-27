c048ecb6ad7a3c8ea647172fc9a556e6b5e38790
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-04-27 23:01:18 +0800

    Update scripts_vote.js

--- js/scripts_vote.js
+++ js/scripts_vote.js
@@ -1,51 +1,45 @@
-window.addEventListener("auxclick", (event) => {
+// document.addEventListener('contextmenu', function(e) { //for right clicks
+//  e.preventDefault();
+// });
+window.addEventListener("auxclick", (event) => { // for middle clicks
    if (event.button === 1) event.preventDefault();
 });
-document.addEventListener('contextmenu', function(e) {
- e.preventDefault();
-});
 
-$(document).keydown(function(e){
+$(document).keydown(function(e){ // for keystrokes
    var url = "https://youtu.be/dQw4w9WgXcQ";
    // var usrl = "#";
-   if (event.keyCode == 123 || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'A'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'S'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'D'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'Q'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'I'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'C'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'J'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'P'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'U'.charCodeAt(0))) { 
+   if (e.keyCode == 123 || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'A'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'S'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'D'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'Q'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'I'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'C'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'J'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'P'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'U'.charCodeAt(0))) { 
       window.location.href = url;
       return false; 
    }
+   // if (event.keyCode == 123) {
+   //    return false; 
+   // }
    // if ((e.ctrlKey || e.shiftKey) && e.keyCode == 'A'.charCodeAt(0)) { 
-   //    window.location.href = url;
    //    return false; 
    // }
    // if ((e.ctrlKey || e.shiftKey) && e.keyCode == 'S'.charCodeAt(0)) { 
-   //    window.location.href = url;
    //    return false; 
    // }
    // if ((e.ctrlKey || e.shiftKey) && e.keyCode == 'D'.charCodeAt(0)) { 
-   //    window.location.href = url;
    //    return false; 
    // }
    // if ((e.ctrlKey || e.shiftKey) && e.keyCode == 'Q'.charCodeAt(0)) { 
-   //    window.location.href = url;
    //    return false; 
    // }
    // if ((e.ctrlKey || e.shiftKey) && e.keyCode == 'I'.charCodeAt(0)) { 
-   //    window.location.href = url;
    //    return false; 
    // }
    // if ((e.ctrlKey || e.shiftKey) && e.keyCode == 'C'.charCodeAt(0)) { 
-   //    window.location.href = url;
    //    return false; 
    // }
    // if ((e.ctrlKey || e.shiftKey) && e.keyCode == 'J'.charCodeAt(0)) { 
-   //    window.location.href = url;
    //    return false; 
    // }
    // if ((e.ctrlKey || e.shiftKey) && e.keyCode == 'P'.charCodeAt(0)) { 
-   //    window.location.href = url;
    //    return false; 
    // }
    // if ((e.ctrlKey || e.shiftKey) && e.keyCode == 'U'.charCodeAt(0)) { 
-   //    window.location.href = url;
    //    return false; 
    // }
 }); 
