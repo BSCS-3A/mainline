8d62b54d19e276e8e809a951601e78c337630f61
DenTitanium <kathrindenisemontallana.taclas@bicol-u.edu.ph>
2021-04-27 22:49:47 +0800

    msg box

--- php/navStudent.php
+++ php/navStudent.php
@@ -25,7 +25,7 @@ date_default_timezone_set('Asia/Manila');
     <link rel="stylesheet" type="text/css" href="../css/student_css/style_studNav.css">
     
     <!-- for disabling inspect element -->
-    <!-- <script src="../js/scripts_vote.js"></script> -->
+    <script src="../js/scripts_vote.js"></script>
     <title>BUCEILS HS Online Voting System</title>
 </head>
 
