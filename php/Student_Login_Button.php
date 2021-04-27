f52495dbf07fe7839a028d4c56db551408845944
JoshuaAlexLlander <79438597+JoshuaAlexLlander@users.noreply.github.com>
2021-04-27 21:51:18 +0800

    added validation for illegal characters in login
    also reconstructed conditions for login and login w/ captcha

--- php/Student_Login_Button.php
+++ php/Student_Login_Button.php
@@ -1,10 +1,13 @@
 <?php
-include ('db_conn.php');
 session_start();
+include ('db_conn.php');
 
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
-        if(isset($_POST['g-recaptcha-response'])) {
+if (isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
+    
+if($_SESSION['incorrectTry']>=3){
+    //login with captcha
+ if(isset($_POST['g-recaptcha-response'])) {
    // RECAPTCHA SETTINGS
    $captcha = $_POST['g-recaptcha-response'];
    $ip = $_SERVER['REMOTE_ADDR'];
@@ -19,29 +22,35 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
    }
    else {
      // die('Your account has been logged as a spammer, you cannot continue!');
-    header("Location: StudentLogin.php?error=Oops! It seems like you logged in incorrectly 3 times. Please verify that you are not a bot by clicking the checkbox before logging in.");	
+     header("Location: ..\index.php?error=Oops! It seems like you logged in incorrectly 3 times. Please verify that you are not a bot by clicking the checkbox before logging in.");	
      exit();
    }
- }//end captcha code
-
-	function validate($data){
-       $data = trim($data);
-	   $data = stripslashes($data);
-	   $data = htmlspecialchars($data);
-	   return $data;
-	}
-//gets the input from the textbox in the StudentLogin.php
-	$username = $conn->real_escape_string(validate($_POST['username']));
-	$pass = $conn->real_escape_string(validate($_POST['password'])); //hash here
-
-	if (empty($username)) {
-		header("Location: ..\index.php?error=User Name is required");
-	    exit();
-	}else if(empty($pass)){
-        header("Location: ..\index.php?error=Password is required");
-	    exit();
-	}else{
+ }else {
+     // die('Your account has been logged as a spammer, you cannot continue!');
+     header("Location: ..\index.php?error=Oops! It seems like you logged in incorrectly 3 times. Please verify that you are not a bot by clicking the checkbox before logging in.");	
+     exit();
+   }
+   
+	function validate($conn, $data){
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		$data = $conn->real_escape_string($data);	//added $conn->real_escape_string: escapes special characters 26/04/2021
+		return $data;
+	 }
+ //gets the input from the textbox in the StudentLogin.php
+	 $username = validate($conn, $_POST['username']);
+	 $pass = validate($conn, $_POST['password']); //hash here
+ 
+	 if (empty($username)) {
+		 header("Location: ..\index.php?error=E-mail is required");
+		 exit();
+	 }else if(empty($pass)){
+		  header("Location: ..\index.php?error=Password is required");
+		 exit();
+	 }else{
 		$sql = "SELECT * FROM student WHERE bumail='$username' AND otp='$pass'";
+
 		$result = mysqli_query($conn, $sql);
 //check if every inputs matched if yes go directly to the dashboard
 		if (mysqli_num_rows($result)===1) {
@@ -51,29 +60,88 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
             	$_SESSION['fname'] = $row['fname'];
 				$_SESSION['lname'] = $row['lname'];
             	$_SESSION['student_id'] = $row['student_id'];
-		$_SESSION['grade_level'] = $row['grade_level']; //added for voting : 03/04/2021 , 11:48am
+				$_SESSION['grade_level'] = $row['grade_level']; //added for voting : 03/04/2021 , 11:48am
             	$_SESSION['timestamp']=time(); //added for time session
 				$student_id = $row['student_id'];
 				date_default_timezone_set('Asia/Manila');
 				$date = date('Y-m-d');
 				$time = date('H:i:s');
                 mysqli_query($conn, "INSERT INTO student_access_log(student_id,activity_description,date,time) VALUES($student_id,'Login','$date','$time')");
-            	header("Location: StudentDashboard.php");
-		        exit();
-            }else{
-                $_SESSION['incorrectTry']++;
-				header("Location: ..\index.php?error=Incorrect BU mail or Password");
-		        exit();
-			}
-		}else{
-		    $_SESSION['incorrectTry']++;
-			header("Location: ..\index.php?error=Incorrect BU mail or Password");
-	        exit();
-		}
-	}
+				header("Location: StudentDashboard.php");
+				 exit();
+			 }else{
+				 $_SESSION['incorrectTry']++;
+				 header("Location: ..\index.php?error=Incorrect BU mail or Password");
+				 exit();
+			 }
+		 }else{
+			 $_SESSION['incorrectTry']++;
+			 header("Location: ..\index.php?error=Incorrect BU mail or Password");
+			 exit();
+		 }
+	 }
 
+}
+else{
+
+	function validate($conn, $data){
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		$data = $conn->real_escape_string($data);	//added $conn->real_escape_string: escapes special characters 26/04/2021
+		return $data;
+	 }
+ //gets the input from the textbox in the StudentLogin.php
+	 $username = validate($conn, $_POST['username']);
+	 $pass = validate($conn, $_POST['password']); //hash here
+ 
+	 if (empty($username)) {
+		 header("Location: ..\index.php?error=E-mail is required");
+		 exit();
+	 }else if(empty($pass)){
+		  header("Location: ..\index.php?error=Password is required");
+		 exit();
+	 }else{
+		$sql = "SELECT * FROM student WHERE bumail='$username' AND otp='$pass'";
+
+		$result = mysqli_query($conn, $sql);
+//check if every inputs matched if yes go directly to the dashboard
+		if (mysqli_num_rows($result)===1) {
+			$row = mysqli_fetch_assoc($result);
+            if ($row['bumail']===$username && $row['otp']===$pass) {
+            	$_SESSION['bumail'] = $row['bumail'];
+            	$_SESSION['fname'] = $row['fname'];
+				$_SESSION['lname'] = $row['lname'];
+            	$_SESSION['student_id'] = $row['student_id'];
+				$_SESSION['grade_level'] = $row['grade_level']; //added for voting : 03/04/2021 , 11:48am
+            	$_SESSION['timestamp']=time(); //added for time session
+				$student_id = $row['student_id'];
+				date_default_timezone_set('Asia/Manila');
+				$date = date('Y-m-d');
+				$time = date('H:i:s');
+                mysqli_query($conn, "INSERT INTO student_access_log(student_id,activity_description,date,time) VALUES($student_id,'Login','$date','$time')");
+				header("Location: StudentDashboard.php");
+				 exit();
+			 }else{
+				 $_SESSION['incorrectTry']++;
+				 header("Location: ..\index.php?error=Incorrect BU mail or Password");
+				 exit();
+			 }
+		 }else{
+			 $_SESSION['incorrectTry']++;
+			 header("Location: ..\index.php?error=Incorrect BU mail or Password");
+			 exit();
+		 }
+	 }
+ 
+    
+}
+    
+ 
 }else{
     $_SESSION['incorrectTry']++;
 	header("Location: ..\index.php"); //header("Location: ..\Login UI v2\html\AdminLogin.php");
 	exit();
 }
+
+?>
