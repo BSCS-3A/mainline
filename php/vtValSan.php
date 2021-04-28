af88eed3ea272faf37c2b407ab7e355f20b97317
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-04-28 09:46:22 +0800

    Update vtValSan.php

--- php/vtValSan.php
+++ php/vtValSan.php
@@ -131,7 +131,7 @@
 
     function redirect($url){
         if (headers_sent()){
-          die('<script type="text/javascript">window.location.replace("'.$url.'");</script‌​>');
+          die('<script type="text/javascript">window.location.href = '.$url.';</script‌​>');
         }else{
           header('Location: ' . $url);
           die();
