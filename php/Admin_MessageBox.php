c0e27e7d6c1a4090d651cc9c716d220c551fffe0
Kaycee Ballesteros <79438209+KayceeBallesteros@users.noreply.github.com>
2021-05-03 21:19:00 +0800

    +warning message

--- php/Admin_MessageBox.php
+++ php/Admin_MessageBox.php
@@ -84,6 +84,15 @@
     <div class="ccheader">
     <h3>MESSAGE BOX</h3>
 </div>
+
+<?php
+  $server = strtolower($_SERVER['HTTP_USER_AGENT']);
+  $isMob = is_numeric(strpos($server, "mobile"));
+  if($isMob){
+    echo "<br><center><p style='color:white';>You are using a mobile device! It is recommended to switch into landscape mode.</p></center>";
+  } 
+?>
+
   <div class="container">  
     <div class="row rounded-lg overflow-hidden shadow">
       <!-- Users box-->
@@ -156,9 +165,10 @@
         </div>
       </div>
     </div>
-    <div class="footer">
+    <!-- <div class="footer">
       <p class="footer-txt">BS COMPUTER SCIENCE 3A © 2021</p>
-  </div>
+  </div> -->
+  <br><br><br><br>
 
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script type="text/javascript"> 
