905bfa5c8f3c80c6e8181b731b2c962134ee1da1
Joshua Codorniz <79438520+CEOofBruh@users.noreply.github.com>
2021-05-07 21:04:20 +0800

    Update Admin_candidate.php

--- php/Admin_candidate.php
+++ php/Admin_candidate.php
@@ -471,7 +471,11 @@
 <div class="btn-toolbar" style="margin-left: 18px;">
   <button class="btn btn-button1 btn-s" data-title="Add" data-toggle="modal" data-target="#add" data-placement="top" id="add-candidate" title="Add"> + Add New Candidate </button>      
 </div>
+    
+    <div class = "container">
+      <div class = "row">
         <div class="col-md-12">
+            <div class ="table-responsive table-body">
             
 <table class= "center" id="datatable" width="100%" cellspacing="0" cellpadding="2px">
                     <thead>
