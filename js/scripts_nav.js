fd5f98e229517e3472f67615ee01fadc5cd166b5
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-05-08 21:05:35 +0800

    Update scripts_nav.js

--- js/scripts_nav.js
+++ js/scripts_nav.js
@@ -25,17 +25,48 @@ document.addEventListener('contextmenu', function(e) { //for right clicks
 $(document).keydown(function(e){ // for keystrokes
    // var usrl = "#";
    // if((e.ctrlKey || e.shiftKey) && (e.keyCode >= 0 || e.keyCode != 116 || e.keyCode != 116))
-   if (e.keyCode == 112 || e.keyCode == 113 || e.keyCode == 112 || e.keyCode == 113 || e.keyCode == 114 || e.keyCode == 115 || ((e.ctrlKey || e.shiftKey) && e.keyCode == 116) || e.keyCode == 117 || e.keyCode == 118 || e.keyCode == 119 || e.keyCode == 120 || e.keyCode == 112 || e.keyCode == 112 || e.keyCode == 112 || e.keyCode == 121 || e.keyCode == 122 || e.keyCode == 123 || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'A'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'S'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'D'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'Q'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'I'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'C'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'J'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'P'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'U'.charCodeAt(0))) {
+   // if (e.keyCode == 112 || e.keyCode == 113 || e.keyCode == 112 || e.keyCode == 113 || e.keyCode == 114 || e.keyCode == 115 || ((e.ctrlKey || e.shiftKey) && e.keyCode == 116) || e.keyCode == 117 || e.keyCode == 118 || e.keyCode == 119 || e.keyCode == 120 || e.keyCode == 112 || e.keyCode == 112 || e.keyCode == 112 || e.keyCode == 121 || e.keyCode == 122 || e.keyCode == 123 || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'A'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'S'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'D'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'Q'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'I'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'C'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'J'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'P'.charCodeAt(0)) || ((e.ctrlKey || e.shiftKey) && e.keyCode == 'U'.charCodeAt(0))) {
+   if (e.keyCode == 112 || 
+      e.keyCode == 113 || 
+      e.keyCode == 112 || 
+      e.keyCode == 113 || 
+      e.keyCode == 114 || 
+      e.keyCode == 115 || 
+      ((e.ctrlKey || e.shiftKey) && e.keyCode == 116) || 
+      e.keyCode == 117 || 
+      e.keyCode == 118 || 
+      e.keyCode == 119 || 
+      e.keyCode == 120 || 
+      e.keyCode == 112 || 
+      e.keyCode == 112 || 
+      e.keyCode == 112 || 
+      e.keyCode == 121 || 
+      e.keyCode == 122 || 
+      e.keyCode == 123 ||
+      (e.ctrlKey && e.keyCode == 'I'.charCodeAt(0)) || 
+      (e.ctrlKey && e.keyCode == 'C'.charCodeAt(0)) || 
+      (e.ctrlKey && e.keyCode == 'J'.charCodeAt(0)) || 
+      (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) || 
+      (e.ctrlKey && e.keyCode == 'P'.charCodeAt(0)) || 
+      ((e.ctrlKey && e.shiftKey) && e.keyCode == 'A'.charCodeAt(0)) || 
+      ((e.ctrlKey && e.shiftKey) && e.keyCode == 'S'.charCodeAt(0)) || 
+      ((e.ctrlKey && e.shiftKey) && e.keyCode == 'D'.charCodeAt(0)) || 
+      ((e.ctrlKey && e.shiftKey) && e.keyCode == 'Q'.charCodeAt(0)) || 
+      ((e.ctrlKey && e.shiftKey) && e.keyCode == 'I'.charCodeAt(0)) || 
+      ((e.ctrlKey && e.shiftKey) && e.keyCode == 'C'.charCodeAt(0)) || 
+      ((e.ctrlKey && e.shiftKey) && e.keyCode == 'J'.charCodeAt(0)) || 
+      ((e.ctrlKey && e.shiftKey) && e.keyCode == 'P'.charCodeAt(0)) || 
+      ((e.ctrlKey && e.shiftKey) && e.keyCode == 'U'.charCodeAt(0))) 
+      {
       // alert("You can't do that here.");
-      // window.location.href = url;
+      window.location.href = url;
       e.preventDefault();
-      document.getElementById('F-modal-message-text').innerHTML = "<h3>Suspicious activity detected! </h3><br> <br>Please refrain from reverse engineering and tampering with this site to avoid the consequences of R.A. 10175.";
-      modal.style.display = "block";
-      document.querySelector("body").style.overflow = 'hidden';
+      // document.getElementById('F-modal-message-text').innerHTML = "<h3>Suspicious activity detected! </h3><br> <br>Please refrain from reverse engineering and tampering with this site to avoid the consequences of R.A. 10175.";
+      // modal.style.display = "block";
+      // document.querySelector("body").style.overflow = 'hidden';
       // return false; 
    }
   
-   
 
    // if (event.keyCode == 123) {
    //    return false; 
