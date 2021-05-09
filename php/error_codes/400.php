2c995148878f35da7fd582b915d5777260240743
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-05-09 12:31:10 +0800

    Error code pages

---
+++ php/error_codes/400.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    	<meta charset="utf-8">
+    	<link rel="icon" href="../../img/BUHS LOGO.png" type="image/png">
+    	<title>400</title>
+		<link rel="stylesheet" type="text/css" href="../../css/student_css/vote_message.css">
+		<link rel="stylesheet" type="text/css" href="../../css/style_login.css">
+	</head>
+
+	<body>
+		<?php
+        echo '<main>
+			<div id="error-message-container" class="error-message-container">			
+				<div id="election-finished-msg" class="error-message">
+				<table style = "width:50%; margin-left: auto; margin-right: auto;">
+					<tr>
+						<th><img src = "../../img/BUHS LOGO.png"></th>
+						<th><h2>400</h2><h3>Bad Request</h3><br><br></th>
+					</tr>
+				</table>
+					
+					<p>Your client has issued a malformed or illegal request.</p>
+				</div>
+
+				<div id="error-button" class="error-button">
+            		<button type="button" id="ok-button" class="OkBTN-error">OK</button>
+          		</div>
+          	</div>
+		</main>';
+        echo '<script>
+        // Get Home button
+        var home = document.getElementById("ok-button");
+
+        home.onclick = function() {
+            location.href = "../../index.php";
+        }
+        </script>';
+		?>
+	</body>
+</html>
