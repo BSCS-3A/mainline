a7c13a0c1928f1bca62038a4ebf8511891dff1d8
Karlo Rhommel C. Sta. Ana <karlorhommelchua.staana@bicol-u.edu.ph>
2021-05-10 07:03:30 +0800

    fix minor interface error

--- php/Admin_MBox.php
+++ php/Admin_MBox.php
@@ -66,12 +66,34 @@ include("db_conn.php");
               echo   '<a href="Admin_MBox.php?id='.$sname[0].'" class="list-group-item list-group-item-action list-group-item-light rounded-0" style="background-color:#1D6986;color:white">                  
               <div class="media-body ml-4" >
               <div class="d-flex align-items-center justify-content-between mb-1" >
-              <p class="cname">'.$sname[0].'</p><small class="small font-weight-bold" >'.$rowsx[2]." ".$rowsx[3].'</small>
+              <p class="cname">'.$sname[0].'</p>
               </div>
-              <p class="cmessage">'.$rowsx[1].'</p>  
+              <p class="cmessage">'.$rowsx[1].'</p>
+              <div class="float-sm-right">
+              <small class="small font-weight-bold" >'.$rowsx[2]." ".$rowsx[3].'</small></div>  
               </div></a>
               ';
             }}}
+            foreach($files as $open){
+            if($open == "." || $open == ".." );
+            else {
+              $sname = explode(".", $open); 
+              $file = file_get_contents("../user/msg/".$sname[0].".html");
+              $rows = explode("##", $file);
+              $nm = count($rows);
+              $rowsx = explode("||",$rows[$nm-2]);
+              if(isset($rows[0]) && $rowsx[4] == "read"){
+                echo   '<a href="Admin_MBox.php?id='.$sname[0].'" class="list-group-item list-group-item-action list-group-item-light rounded-0">                  
+                <div class="media-body ml-4">
+                <div class="d-flex align-items-center justify-content-between mb-1">
+                <p class="cname">'.$sname[0].'</p>                
+                </div>
+                <p class="cmessage">'.$rowsx[1].'</p>
+                <div class="float-sm-right">
+                <small class="small font-weight-bold" >'.$rowsx[2]." ".$rowsx[3].'</small></div>  
+                </div></a>
+                ';
+             }}}
             ?>
             <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
             <script type="text/javascript"> 
