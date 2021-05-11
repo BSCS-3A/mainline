f15f996b8858d276c9aa65fea22b14cc567e9472
lizpen <79520632+lizpen@users.noreply.github.com>
2021-05-11 22:58:25 +0800

    Tie Breaker files
    Submit button, confirm prob

---
+++ php/Admin_tieBreaker.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="../img/BUHS LOGO.png" type="image/png">
+    <!-- <link rel="stylesheet" href="../css/student_css/bootstrap-vote.css">
+    <link rel="stylesheet" href="../css/student_css/font-awesome_vote.css">
+    <link rel="stylesheet" type="text/css" href="../css/student_css/bootstrap.min-vote.css">
+    <link rel="stylesheet" type="text/css" href="../css/student_css/bootstrap_vote.css"> -->
+    <link rel="stylesheet" type="text/css" href="../css/student_css/vote_ballot.css">
+    <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="../js/scripts.js"></script>
+    <title>BUCEILS HS Vote</title>
+</head>
+
+<body>
+    <?php
+        require 'connect.php'; // Remove this when compiling
+        require 'Admin_vtValSan.php';
+        require 'Admin_vtFetch.php';
+        include './backMonitor/fetch_date.php';
+                     if($current_date_time>$last_election_date){
+                        //include 'navAdmin.php';
+                        echo '<header id="F-header"  style="text-align: center;"><b>TIE BREAKER</b></header><br>';
+                        echo '<main>';
+                        echo '<form id = "main-form" method="POST" action = "vtReceipt.php" class="vtBallot" id="vtBallot"><div id="voting-page">';
+
+                        if($_SESSION['admin_position']=="Head Admin")
+                        {
+                          $count_query = "SELECT * FROM student";
+                          $count_res = mysqli_query($conn, $count_query);
+                          $total = mysqli_num_rows($count_res);
+                          
+                          $QoutaAll = floor($total/2)+1; //this is the Quota for all candidate except the representatives
+                          
+                           $val = $conn->query('SELECT 1 from temp_candidate LIMIT 1');
+
+                                  if($val !== FALSE)
+                                  {
+                                      $sql2 = "DROP TABLE temp_candidate";
+                                      $conn->query($sql2);
+                                  }
+                                  else
+                                  {
+                                    //do nothing
+                                  }
+                                  
+                           $samp = "SELECT position_id, COUNT(position_id), total_votes, COUNT(total_votes) FROM candidate GROUP BY position_id, total_votes HAVING COUNT(position_id)>1 AND COUNT(total_votes)>1";
+                                $display_res = $conn->query($samp);
+                                 if($display_res->num_rows>0){
+                                     while($row = $display_res->fetch_assoc())
+                               {
+                            //echo $row['total_votes']." position id :".$row['position_id']."<br>";
+                                 $samp2 = "SELECT * FROM ((candidate INNER JOIN student ON candidate.student_id = student.student_id) INNER JOIN candidate_position ON candidate.position_id = candidate_position.position_id) where candidate.position_id = ".$row['position_id']." AND total_votes = ".$row['total_votes']." ORDER BY candidate_position.heirarchy_id";
+
+                                      $sql = "CREATE TABLE `temp_candidate` (
+                                      `candidate_id` int(11) NOT NULL,
+                                      `student_id` int(11) NOT NULL,
+                                      `position_id` int(11) NOT NULL,
+                                      `total_votes` int(11) NOT NULL,
+                                      `party_name` varchar(30) NOT NULL,
+                                      `platform_info` varchar(100) NOT NULL,
+                                      `credentials` varchar(500) NOT NULL,
+                                      `photo` varchar(30) NOT NULL
+                                    )";
+                                     $conn->query($sql);
+
+
+
+                                 $display_res2 = $conn->query($samp2);
+                                   while($row1 = $display_res2->fetch_assoc()){
+
+                                    if($row1['vote_allow']==1){//for non-representative
+                                      if($row1['total_votes']>=$QoutaAll)
+                                      {
+                                       $insert_data = 'INSERT INTO temp_candidate (candidate_id, student_id, position_id, total_votes, party_name, platform_info, credentials, photo) VALUES ("'.$row1['candidate_id'].'","'.$row1['student_id'].'","'.$row1['position_id'].'","'.$row1['total_votes'].'","'.$row1['party_name'].'","'.$row1['platform_info'].'","'.$row1['credentials'].'","'.$row1['photo'].'")';
+
+                                       $conn->query($insert_data);
+                                      }
+
+                                    }
+                                    else{//for representative
+                                       $count_query1 = "SELECT * FROM student where grade_level = ".$row1['grade_level']."";
+                                       $count_res1 = mysqli_query($conn, $count_query1);
+                                       $total1 = mysqli_num_rows($count_res1);
+                          
+                                      $QoutaAll1 = floor($total1/2)+1;
+                                     
+
+                                      if($row1['total_votes']>=$QoutaAll1)
+                                      {
+                                         $insert_data1 = 'INSERT INTO temp_candidate (candidate_id, student_id, position_id, total_votes, party_name, platform_info, credentials, photo) VALUES ("'.$row1['candidate_id'].'","'.$row1['student_id'].'","'.$row1['position_id'].'","'.$row1['total_votes'].'","'.$row1['party_name'].'","'.$row1['platform_info'].'","'.$row1['credentials'].'","'.$row1['photo'].'")';
+
+                                         $conn->query($insert_data1);
+                                      }
+
+
+
+                                      
+                                   }
+                                    
+                              }
+                            }
+                          }
+                        $table = $conn->query("SELECT * FROM ((temp_candidate INNER JOIN student ON temp_candidate.student_id = student.student_id) INNER JOIN candidate_position ON temp_candidate.position_id = candidate_position.position_id) ORDER BY candidate_position.heirarchy_id"); 
+
+                        generateBallot($table);
+                        require 'Admin_vtConfirm.php';
+                        echo '</div>';
+                        echo '<div id="vote-button"><button id="vote-btn" name = "vote-button" class="vote-btn" type = "button">SUBMIT</button></div>
+                        </form>';
+                        echo '</main>';
+                        echo '<br>
+                        <script src = "../js/modals.js"></script>';
+                      }
+                    
+                  }else{
+                      //post something here like election not yet over
+                  }
+             
+    ?>
+ </body>
+
+</html>
---
+++ php/Admin_vtSubmit.php
@@ -0,0 +1,89 @@
+<?php
+	$table = $conn->query("SELECT * FROM ((candidate INNER JOIN student ON candidate.student_id = student.student_id) INNER JOIN candidate_position ON candidate.position_id = candidate_position.position_id) ORDER BY candidate_position.heirarchy_id"); // get positions
+	
+	$choice_final = array();
+	$vote_table = array();
+
+	// Sanitation and Validation
+	mysqli_data_seek($table, 0);
+	//$stud_id = $_SESSION['student_id'];
+	$status = "";
+	while($poss = $table->fetch_assoc()){   // loop through all positions
+		if(($poss["vote_allow"] == 0 && $_SESSION['admin_position'] == "Head Admin") || $poss["vote_allow"] == 1){
+			if(empty(($_POST[$poss['heirarchy_id']]))){
+				$choice = 0;
+			}
+			else{
+				$_POST[$poss['heirarchy_id']] = filter_var($_POST[$poss['heirarchy_id']], FILTER_SANITIZE_STRING);;
+				// $choice = $_POST[$poss['heirarchy_id']];
+				$choice = cleanInput($_POST[$poss['heirarchy_id']]);
+			}
+
+			//if(isValidCandidate($conn, $choice, $poss['heirarchy_id'])){
+				if($poss['candidate_id'] == $choice){
+					$choice_final[$poss['heirarchy_id']] = $poss['candidate_id'];
+					$status = "Voted";
+				}
+				else{
+					$status = "Abstain";
+				}
+			//}
+			/*else{
+				// notify admin & return to ballot
+				// echo "Ballot tampering detected.";
+				errorMessage("Ballot tampering detected. You have been banned from voting and reported to the admins.");
+				// Submit records marked invalid
+				mysqli_data_seek($table, 0);
+				while($poss = $table->fetch_assoc()){
+					$cand_id = $conn->real_escape_string($poss['candidate_id']);
+					$stud_id = $conn->real_escape_string($stud_id);
+					$conn->query("INSERT INTO `vote` (`vote_log_id`, `student_id`, `candidate_id`, `status`, `time_stamp`) VALUES (NULL, $stud_id, $cand_id, 'Invalid', current_timestamp())");
+				}
+				// update voter's status
+				//$conn->query("UPDATE `student` SET `voting_status` = true WHERE `student`.`student_id` = '$stud_id'");
+				// notify admin
+				notifyAdmin("Warning: An attempt to submit a tampered ballot was made! The user was automatically banned from voting.");
+				exit();
+				// header("Location: index.php");
+			}*/
+		}
+		else{
+			$status = " Abstain";
+		}
+		$vote_table[$poss['candidate_id']] = $status;
+	}
+
+	foreach($choice_final as $value){
+		$candidate = $conn->real_escape_string($value);
+		$conn->query("UPDATE candidate SET total_votes = total_votes + 1 WHERE candidate.candidate_id = $candidate");	
+	}
+
+	// Submission
+	// mysqli_data_seek($table, 0);
+	// while($poss = $table->fetch_assoc()){
+	// 	// echo $poss['heirarchy_id'];
+	// 	if(($poss["vote_allow"] == 0 && $_SESSION['grade_level'] == $poss["grade_level"]) || $poss["vote_allow"] == 1){
+	// 		$choice = $choice_final[$poss['heirarchy_id']];
+	// 		if($poss['candidate_id'] == $choice){
+	// 			$candidate = $conn->real_escape_string($poss['candidate_id']);
+	// 			// $candidate = $poss['candidate_id'];
+	// 			$conn->query("UPDATE candidate SET total_votes = total_votes + 1 WHERE candidate.candidate_id = $candidate");	
+	// 		}
+	// 	}
+	// }
+	
+	mysqli_data_seek($table, 0);
+	while($poss = $table->fetch_assoc()){
+		$status = $conn->real_escape_string($vote_table[$poss['candidate_id']]);
+		$cand_id = $conn->real_escape_string($poss['candidate_id']);
+		//$stud_id = $conn->real_escape_string($stud_id);
+		//$conn->query("INSERT INTO `vote` (`vote_log_id`, `student_id`, `candidate_id`, `status`, `time_stamp`) VALUES (NULL, $stud_id, $cand_id, '$status', current_timestamp())");
+	}
+
+	$sql2 = "DROP TABLE temp_candidate";
+    $conn->query($sql2);
+	
+
+	// update voter's status
+	// $conn->query("UPDATE `student` SET `voting_status` = true WHERE `student`.`student_id` = '$stud_id'");
+?>
