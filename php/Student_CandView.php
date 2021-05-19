7d470a38992233f9f347d88421f60a8602991474
Joshua Codorniz <79438520+CEOofBruh@users.noreply.github.com>
2021-05-19 19:14:11 +0800

    scroll to top function

--- php/Student_CandView.php
+++ php/Student_CandView.php
@@ -176,32 +176,23 @@
             BS COMPUTER SCIENCE 3A © 2021
             </footer> -->
 
-    <script>
-        /*$('.icon').click(function () {
+     <script>
+        $('.icon').click(function () {
             $('span').toggleClass("cancel");
-var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
-var rootElement = document.documentElement;
 
-function handleScroll() {
-  // Do something on scroll
-  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
-  if ((rootElement.scrollTop / scrollTotal ) > 0.50) {
-    // Show button
-    scrollToTopBtn.classList.add("showBtn");
-  } else {
-    // Hide button
-    scrollToTopBtn.classList.remove("showBtn");
-  }
-}
+    </script>
+    <script>
+      var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+      var rootElement = document.documentElement
 
-function scrollToTop() {
-  rootElement.scrollTo({
+    function scrollToTop() {
+  // Scroll to top logic
+    rootElement.scrollTo({
     top: 0,
     behavior: "smooth"
-  });
+  })
 }
-scrollToTopBtn.addEventListener("click", scrollToTop);
-document.addEventListener("scroll", handleScroll);*/
+scrollToTopBtn.addEventListener("click", scrollToTop)
     </script>
 </body>
 
