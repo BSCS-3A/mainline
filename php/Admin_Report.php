4071df7b9a2092f77bc49e52d02f5ffea85150aa
lizpen <79520632+lizpen@users.noreply.github.com>
2021-05-20 12:49:33 +0800

    Display fixes

--- php/Admin_Report.php
+++ php/Admin_Report.php
@@ -55,114 +55,142 @@ include("db_conn.php");
       {
         if($current_date_time>$last_election_date)
         {
-          echo '<header id="F-header"  style="text-align: center;"><b>TIE BREAKER</b></header><br>';
-          echo '<main>';
-          echo '<form id = "main-form" method="POST" action = "Admin_vtReceipt.php" class="vtBallot" id="vtBallot"><div id="voting-page">';
+          $tie_position_counter = 0;
 
-          if($_SESSION['admin_position']=="Head Admin")
+          $count_student = "SELECT * FROM student";
+          $count_result = mysqli_query($conn, $count_student);
+          $total = mysqli_num_rows($count_result);
+                            
+          $QoutaAll = floor($total/2)+1; //this is the Quota for all candidate except the representatives
+                            
+          $val = $conn->query('SELECT 1 from temp_tie LIMIT 1');// checks if table is already created
+
+          if($val !== FALSE)// if true do drop 
           {
-            $count_student = "SELECT * FROM student";
-            $count_result = mysqli_query($conn, $count_student);
-            $total = mysqli_num_rows($count_result);
-                            
-            $QoutaAll = floor($total/2)+1; //this is the Quota for all candidate except the representatives
-                            
-            $val = $conn->query('SELECT 1 from temp_tie LIMIT 1');// checks if table is already created
-
-              if($val !== FALSE)// if true do drop 
-              {
-                $tbDrop = "DROP TABLE temp_tie";
-                $conn->query($tbDrop);
-              }
-             //using query, retrieve all the data from candidate that have ties then group them by position id and total votes for easy data and code manipulation                        
-            $tieGet = "SELECT position_id, COUNT(position_id), total_votes, COUNT(total_votes) FROM candidate GROUP BY position_id, total_votes HAVING COUNT(position_id)>1 AND COUNT(total_votes)>1";
-            $display_res = $conn->query($tieGet);
+            $tbDrop = "DROP TABLE temp_tie";
+            $conn->query($tbDrop);
+          }
             
-            if($display_res->num_rows>0)
+          //using query, retrieve all the data from candidate that have ties then group them by position id and total votes for easy data and code manipulation                        
+          $tieGet = "SELECT position_id, COUNT(position_id), total_votes, COUNT(total_votes) FROM candidate GROUP BY position_id, total_votes HAVING COUNT(position_id)>1 AND COUNT(total_votes)>1";
+          $display_res = $conn->query($tieGet);
+            
+          if($display_res->num_rows>0)
+          {
+            tempCandidate($conn);// create a temp candidate table
+            while($row = $display_res->fetch_assoc())
             {
-              tempCandidate($conn);// create a temp candidate table
-              while($row = $display_res->fetch_assoc())
+             //this will filter the retrieve done in tieGet by getting all the tied candidates individually
+              $tieGet2 = "SELECT * FROM ((candidate INNER JOIN student ON candidate.student_id = student.student_id) INNER JOIN candidate_position ON candidate.position_id = candidate_position.position_id) where candidate.position_id = ".$row['position_id']." AND total_votes = ".$row['total_votes']." ORDER BY candidate_position.heirarchy_id";
+
+              //creates a temp table that will hold the data of tied candidates
+              //note temp tables are uneditable nor updatable 
+              $sql = "CREATE TABLE `temp_tie` (
+                `candidate_id` int(11) NOT NULL,
+                `student_id` int(11) NOT NULL,
+                `position_id` int(11) NOT NULL,
+                `total_votes` int(11) NOT NULL,
+                `party_name` varchar(30) NOT NULL,
+                `platform_info` varchar(100) NOT NULL,
+                `credentials` varchar(500) NOT NULL,
+                `photo` varchar(100) NOT NULL
+              )";
+              $conn->query($sql);
+
+              $display_res2 = $conn->query($tieGet2);
+              while($row1 = $display_res2->fetch_assoc())
               {
-               //this will filter the retrieve done in tieGet by getting all the tied candidates individually
-                $tieGet2 = "SELECT * FROM ((candidate INNER JOIN student ON candidate.student_id = student.student_id) INNER JOIN candidate_position ON candidate.position_id = candidate_position.position_id) where candidate.position_id = ".$row['position_id']." AND total_votes = ".$row['total_votes']." ORDER BY candidate_position.heirarchy_id";
+                $pos_hold = $row1['position_id']; // hold pos id
 
-                //creates a temp table that will hold the data of tied candidates
-                //note temp tables are uneditable nor updatable 
-                $sql = "CREATE TABLE `temp_tie` (
-                  `candidate_id` int(11) NOT NULL,
-                  `student_id` int(11) NOT NULL,
-                  `position_id` int(11) NOT NULL,
-                  `total_votes` int(11) NOT NULL,
-                  `party_name` varchar(30) NOT NULL,
-                  `platform_info` varchar(100) NOT NULL,
-                  `credentials` varchar(500) NOT NULL,
-                  `photo` varchar(100) NOT NULL
-                )";
-                $conn->query($sql);
-
-                $display_res2 = $conn->query($tieGet2);
-                while($row1 = $display_res2->fetch_assoc())
-                {
-                  $pos_hold = $row1['position_id']; // hold pos id
-
-                  //checks first if candidate is representative or not
-                  if($row1['vote_allow']==1)
-                  {//for non-representative
-                    if($row1['total_votes']>=$QoutaAll)//checks if they met the quota
+                //checks first if candidate is representative or not
+                if($row1['vote_allow']==1)
+                {//for non-representative
+                  if($row1['total_votes']>=$QoutaAll)//checks if they met the quota
+                  {
+                    //get the highest vote from their position, then compare if matches
+                    //if matches then its a legit tie
+                    //it also patches the loophole of tied candidates display
+                    if(getMax($conn, $pos_hold)==$row1['total_votes'])
                     {
-                      //get the highest vote from their position, then compare if matches
-                      //if matches then its a legit tie
-                      //it also patches the loophole of tied candidates display
-                      if(getMax($conn, $pos_hold)==$row1['total_votes'])
-                      {
-                        //insert into temp_tie table
+                      //insert into temp_tie table
                         //note temp tables can only accept insert 
-                        $insert_data = 'INSERT INTO temp_tie (candidate_id, student_id, position_id, total_votes, party_name, platform_info, credentials, photo) VALUES ("'.$row1['candidate_id'].'","'.$row1['student_id'].'","'.$row1['position_id'].'","'.$row1['total_votes'].'","'.$row1['party_name'].'","'.$row1['platform_info'].'","'.$row1['credentials'].'","'.$row1['photo'].'")';
+                      $insert_data = 'INSERT INTO temp_tie (candidate_id, student_id, position_id, total_votes, party_name, platform_info, credentials, photo) VALUES ("'.$row1['candidate_id'].'","'.$row1['student_id'].'","'.$row1['position_id'].'","'.$row1['total_votes'].'","'.$row1['party_name'].'","'.$row1['platform_info'].'","'.$row1['credentials'].'","'.$row1['photo'].'")';
 
-                          $conn->query($insert_data);
-                      }
+                        $conn->query($insert_data);
+                        $tie_position_counter++;
                     }
                   }
-                  else
-                  {//for representative
-                    /*=======gets the quota by their grade levels==========*/
-                    $count_query1 = "SELECT * FROM student where grade_level = ".$row1['grade_level']."";
-                    $count_res1 = mysqli_query($conn, $count_query1);
-                    $total1 = mysqli_num_rows($count_res1);
-                            
-                    $QoutaAll1 = floor($total1/2)+1;
-                    /*==========END==============*/              
-                    if($row1['total_votes']>=$QoutaAll1)
+                }
+                else
+                {//for representative
+                  /*=======gets the quota by their grade levels==========*/
+                  $count_query1 = "SELECT * FROM student where grade_level = ".$row1['grade_level']."";
+                  $count_res1 = mysqli_query($conn, $count_query1);
+                  $total1 = mysqli_num_rows($count_res1);
+                          
+                  $QoutaAll1 = floor($total1/2)+1;
+                  /*==========END==============*/              
+                  if($row1['total_votes']>=$QoutaAll1)
+                  {
+                    if(getMax($conn, $pos_hold)==$row1['total_votes'])
                     {
-                      if(getMax($conn, $pos_hold)==$row1['total_votes'])
-                      {
-                        $insert_data1 = 'INSERT INTO temp_tie (candidate_id, student_id, position_id, total_votes, party_name, platform_info, credentials, photo) VALUES ("'.$row1['candidate_id'].'","'.$row1['student_id'].'","'.$row1['position_id'].'","'.$row1['total_votes'].'","'.$row1['party_name'].'","'.$row1['platform_info'].'","'.$row1['credentials'].'","'.$row1['photo'].'")';
+                      $insert_data1 = 'INSERT INTO temp_tie (candidate_id, student_id, position_id, total_votes, party_name, platform_info, credentials, photo) VALUES ("'.$row1['candidate_id'].'","'.$row1['student_id'].'","'.$row1['position_id'].'","'.$row1['total_votes'].'","'.$row1['party_name'].'","'.$row1['platform_info'].'","'.$row1['credentials'].'","'.$row1['photo'].'")';
 
-                        $conn->query($insert_data1);
-                      }
+                      $conn->query($insert_data1);
+                      $tie_position_counter++;
                     }
                   }
                 }
               }
             }
-            //after all those process finally, we can send filtered data to generateBallot
-            $table = $conn->query("SELECT * FROM ((temp_tie INNER JOIN student ON temp_tie.student_id = student.student_id) INNER JOIN candidate_position ON temp_tie.position_id = candidate_position.position_id) ORDER BY candidate_position.heirarchy_id"); 
-
-            generateBallot($table);
-            require 'Admin_vtConfirm.php';
-            echo '</div>';
-            echo '<div id="vote-button"><button id="vote-btn" name = "vote-button" class="vote-btn" type = "button">SUBMIT</button></div>
-                          </form>';
-            echo '</main>';
-            echo '<br>
-            <script src = "../js/modals.js"></script>';
-          
-          // you can put the button of archive and pdf here
-          }//end of head admin session check
-          else
-          {
-            include '../html/admin_tiebreaker_prompt.html';
           }
+
+          //after all those process finally, we can send filtered data to generateBallot
+          $table = $conn->query("SELECT * FROM ((temp_tie INNER JOIN student ON temp_tie.student_id = student.student_id) INNER JOIN candidate_position ON temp_tie.position_id = candidate_position.position_id) ORDER BY candidate_position.heirarchy_id"); 
+
+          if ($tie_position_counter == 0)
+          {
+?>
+            <br><br><br><br>
+
+            <!-- check if archive has been clicked once -->
+            <div class="Bbtn_save">
+              <button name='save_btn' onclick="parent.open('Admin_generate-pdf.php')" class='Bbtn_save2arc'"><b>SAVE TO ARCHIVES</b></button>
+            </div>
+
+            <br>
+
+            <!-- cannot be clicked if archive has not been clicked yet -->
+            <div class="Bbtn_dl">
+              <button name='dl_btn' onclick="parent.open('Admin_generate-pdf.php')" class='Bbtn_dlreport'"><b>DOWNLOAD PDF</b></button>
+            </div>
+
+<?php
+          }
+          else
+          {  // start tie display
+            if($_SESSION['admin_position']=="Head Admin")
+            {
+              echo '<header id="F-header"  style="text-align: center;"><b>TIE BREAKER</b></header><br>';
+              echo '<main>';
+              echo '<form id = "main-form" method="POST" action = "Admin_vtReceipt.php" class="vtBallot" id="vtBallot"><div id="voting-page">';
+
+              generateBallot($table);
+              require 'Admin_vtConfirm.php';
+              echo '</div>';
+              echo '<div id="vote-button"><button id="vote-btn" name = "vote-button" class="vote-btn" type = "button">SUBMIT</button></div>
+                            </form>';
+              echo '</main>';
+              echo '<br>
+              <script src = "../js/modals.js"></script>';
+            
+            // you can put the button of archive and pdf here
+            }//end of head admin session check
+            else
+            {
+              include '../html/admin_tiebreaker_prompt.html';
+            }
+          } // end tie display
         }
         else
         {
