f47f5a5e2171b1ba098c9802d9613bb7a1bb5315
Kerby Maravilla Bartolay <70627486+kerbae@users.noreply.github.com>
2021-05-22 23:39:46 +0800

    Update backFun_delEsig.php

--- php/backAdmin/backFun_delEsig.php
+++ php/backAdmin/backFun_delEsig.php
@@ -9,6 +9,10 @@ $blank = "";
 $query = "UPDATE admin SET esignature = '$blank' WHERE admin_id = '$sigid'";
 $query_run = mysqli_query($connection,$query);
 if($query_run){
+  //For Logs
+  $_SESSION['action'] = 'deleted E-Signatory : ' . $sigid ;
+  include 'backFun_actLogs_v0_1.php';
+  
   echo"<script language='javascript'>
   alert('E-Signature has been Deleted');
   window.location.href = '../Admin_signConfig.php';
