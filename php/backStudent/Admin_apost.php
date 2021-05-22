e57714beb80716c7ceb548552acbb22178bf3c1b
KRcsa <karlorhommelchua.staana@bicol-u.edu.ph>
2021-05-22 22:49:04 +0800

    Fix MBox Security issue
    Added strip_tags

--- php/backStudent/Admin_apost.php
+++ php/backStudent/Admin_apost.php
@@ -4,6 +4,7 @@ if(isset($_POST['text'])){
     if($text != ""){
         $text_message = "0||".$text."||".date('h:i')."||".date('Y/m/d')."||read"."##\n";
         $file = "../../user/msg/".$_POST['idup'].".html";
+        $text_message = strip_tags($text_message);
         file_put_contents($file, $text_message, FILE_APPEND | LOCK_EX);
     }
 }
--- php/backStudent/Student_post.php
+++ php/backStudent/Student_post.php
@@ -4,6 +4,7 @@ if(isset($_POST['text'])){
     if($text != ""){
         $text_message = "1||".$text."||".date('h:i')."||".date('Y/m/d')."||unread"."##\n";
         $file = "../../user/msg/".$_POST['idup'].".html";
+        $text_message = strip_tags($text_message);
         file_put_contents($file, $text_message, FILE_APPEND | LOCK_EX);
     }
 }
