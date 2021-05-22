4fdc5c86f49ea37adcf228ccf7153f4a42fff394
ayenrogero <70675401+ayenrogero@users.noreply.github.com>
2021-05-22 21:27:45 +0800

    election results stud side - revert

--- php/Student_ElectRes.php
+++ php/Student_ElectRes.php
@@ -241,12 +241,12 @@
             foreach($candidates as $candidate){
                 echo '<div class="Bposition">';
                 if(empty($candidate['photo'])){
-                  echo '<img src="../img/admin_anon.png" width="40px" height="40px"/>';
+                  echo '<img src="../img/admin_anon.png" width="50px" height="50px"/>';
                 }else{
-                  echo '<img src="'.$candidate['photo'].'" width="40px" height="40px"/>';
+                  echo '<img src="'.$candidate['photo'].'" width="50px" height="50px"/>';
                 }
                 echo '<p class="Bnum_votes">TOTAL VOTES: '.$candidate['total_votes'].'</p>';
-                echo '<p class="Bname">'.$candidate['first_name'].' '.$candidate['middle_name'].' '.$candidate['last_name'].'</p>';
+                echo '<p class="Bname">'.$candidate['first_name'].' '.$candidate['middle_name'].' '.$candidate['last_name'].' </p>';
                 echo '<p class="Bposition">'.$candidate['position'].'</p>';
                 echo '<p class="Bparty">'.$candidate['party_name'].'</p>';
                 echo '</div>';
