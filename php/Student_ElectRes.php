1a312b1404703f0c29d2e5660f8ec9d4f826256e
iveejntln <65966844+iveejntln@users.noreply.github.com>
2021-05-23 22:11:18 +0800

    removed undefined variable

--- php/Student_ElectRes.php
+++ php/Student_ElectRes.php
@@ -251,7 +251,7 @@
                 }
                 echo '<div class ="Bcan">';
                     if(empty($candidate['middle_name'])){
-                        echo '<b>'.$candidate['first_name'].' '.$candidate['last_name'].' ('.$evr_candidate['party_name'].')<span>'.$evr_candidate['total_votes'].'</span></b>';
+                        echo '<b>'.$candidate['first_name'].' '.$candidate['last_name'].' ('.$candidate['party_name'].')<span>'.$candidate['total_votes'].'</span></b>';
                     }else{
                         echo '<b>'.$candidate['first_name'].' '.$candidate['middle_name'][0].'. '.$candidate['last_name'].' ('.$candidate['party_name'].')<span>'.$candidate['total_votes'].'</span></b>';
                     }
