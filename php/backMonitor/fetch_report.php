cccccebc58dcc308995764596377d062260abbd1
iveejntln <65966844+iveejntln@users.noreply.github.com>
2021-05-24 21:03:19 +0800

    Revert "added condition if vote event is empty"
    This reverts commit b42224bacf44e12f81d323dab8a1bfc64b50126a.

--- php/backMonitor/fetch_report.php
+++ php/backMonitor/fetch_report.php
@@ -1,16 +1,13 @@
 <?php
 	require 'db_conn.php';
+
     // Report and PDF date/time
     $sched_row = $conn->query("SELECT * FROM `vote_event` ORDER BY `vote_event_id` DESC LIMIT 1");
     $sched = $sched_row->fetch_assoc();
-    
-    if($sched != NULL){
-        $start_time = strtotime($sched['start_date']);
-        $end_time = strtotime($sched['end_date']);
-        $access_time = time();
-        $end_date = new DateTime($sched['end_date']);
-    }    
-
+    $start_time = strtotime($sched['start_date']);
+    $end_time = strtotime($sched['end_date']);
+    $access_time = time();
+    $end_date = new DateTime($sched['end_date']);
     $archive_sql = ($conn->query("SELECT * FROM `archive` ORDER BY `archive_id` DESC LIMIT 1"));
     $archive_row = $archive_sql->fetch_assoc();
     $archive_sy = $archive_row['school_year'];
@@ -80,33 +77,28 @@
     //======create temporary table==========
     //additional notes: --temporary tables cant be edit nor update its like backup data, it is always static
       function tempCandidate($conn)
-              {
+      {
+          $val = $conn->query('SELECT 1 from temp_candidate LIMIT 1');
 
-                  $val = $conn->query('SELECT 1 from temp_candidate LIMIT 1');
+          if($val == FALSE)
+          {
+              $sql = "CREATE TABLE `temp_candidate`(
+                      `candidate_id` int(11) NOT NULL,
+                      `student_id` int(11) NOT NULL,
+                      `position_id` int(11) NOT NULL,
+                      `total_votes` int(11) NOT NULL,
+                      `party_name` varchar(30) NOT NULL,
+                      `platform_info` varchar(100) NOT NULL,
+                      `credentials` varchar(500) NOT NULL,
+                      `photo` varchar(100) NOT NULL
+              )";
 
-                      if($val != FALSE)
-                          {
-                               $drp = "DROP TABLE temp_candidate";
-			      	$conn->query($drp);
-                               
-                          }
-				 $sql = "CREATE TABLE `temp_candidate` (
-                                 `candidate_id` int(11) NOT NULL,
-                                  `student_id` int(11) NOT NULL,
-                                   `position_id` int(11) NOT NULL,
-                                   `total_votes` int(11) NOT NULL,
-                                   `party_name` varchar(30) NOT NULL,
-                                   `platform_info` varchar(100) NOT NULL,
-                                  `credentials` varchar(500) NOT NULL,
-                                    `photo` varchar(100) NOT NULL
-                                    )";
+            $conn->query($sql);
 
-                                   $conn->query($sql);
-
-                                    $cpydata = "INSERT INTO temp_candidate SELECT * FROM candidate";
-                                   $conn->query($cpydata);
-			
-              }
+            $cpydata = "INSERT INTO temp_candidate SELECT * FROM candidate";
+            $conn->query($cpydata);
+          } 
+			}
 
     //=========store the final result to archive table===============
     //additional notes: if there is a tie, the tie should be resolve first before calling this function.
