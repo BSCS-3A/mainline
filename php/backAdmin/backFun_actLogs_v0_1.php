44de3c5f42e464299608c31d1bd760ee59a9344a
Kerby Maravilla Bartolay <70627486+kerbae@users.noreply.github.com>
2021-05-24 10:09:56 +0800

    Editing ng path para sa pagconnect sa db.

--- php/backAdmin/backFun_actLogs_v0_1.php
+++ php/backAdmin/backFun_actLogs_v0_1.php
@@ -9,7 +9,7 @@ use of session for admin_id
 	//$_SESSION['action'] = 'Added an Account' or 'Updated an Account' or 'Set and Sent Reminder' or 'Sent last Reminder' or 'Set Signatories' or 'Set Election Time and Date'
 
 	// Create connection
-	require '../db_conn.php';
+	require './db_conn.php';
 
 	$activity_description = $_SESSION['action'];
 	unset($_SESSION['action']);
