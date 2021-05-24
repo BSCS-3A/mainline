589623bba136cf6bc1abc8a4308b619d1a4a4206
LowSpec-Coder-2nd <79438711+LowSpec-Coder-2nd@users.noreply.github.com>
2021-05-24 11:50:11 +0800

    fix archive function
    fix code loop hole in the code

--- php/backMonitor/fetch_report.php
+++ php/backMonitor/fetch_report.php
@@ -109,7 +109,7 @@
     //additional notes: if there is a tie, the tie should be resolve first before calling this function.
       function Archive($conn, $school_year)
       {
-        $sql = "SELECT candidate.student_id, student.student_id, grade_level, position_name, platform_info, vote_allow, candidate.position_id, candidate_position.position_id, Max(total_votes) AS total_votes from (candidate inner join candidate_position on candidate.position_id = candidate_position.position_id) inner join student where candidate.student_id = student.student_id group by candidate.position_id order by heirarchy_id";
+       $sql = 'SELECT position_id, max(total_votes) as ttl_votes from candidate group by position_id';
 
         $getlist = $conn->query($sql);
         $count_query = "SELECT * FROM student";
@@ -119,37 +119,36 @@
 
         while($row = $getlist->fetch_assoc())
         {
-          if($row['vote_allow']==1)
-          {
-            if($row['total_votes']>=$QoutaAll)
-            {
-              $sql2 = "SELECT * from student where student_id= ".$row['student_id']."";
-              $q1 = $conn->query($sql2);
-              while($row1 = $q1->fetch_assoc())
-              {
-                  $sql_insert = "INSERT INTO archive (position_name, winner_fname, winner_mname, winner_lname, school_year, platform_info) VALUES ('".$row['position_name']."','".$row1['fname']."','".$row1['Mname']."','".$row1['lname']."','".$school_year."','".$row['platform_info']."')";
-                $conn->query($sql_insert);
-              }
-            }
-          }
-          else
-          {
-            $count_query1 = "SELECT * FROM student where grade_level = ".$row['grade_level']."";
-            $count_res1 = mysqli_query($conn, $count_query1);
-            $total1 = mysqli_num_rows($count_res1);
+		$accur_data = 'SELECT * from (candidate inner join candidate_position on candidate.position_id = candidate_position.position_id) inner join student on candidate.student_id = student.student_id where candidate.position_id = '.$row['position_id'].' and total_votes = '.$row['ttl_votes'].'';
+		$getdata = $conn->query($accur_data);
+		while($row1 = $getdata->fetch_assoc())
+		{
+			 if($row1['vote_allow']==1)
+        			  {
+           				 if($row1['total_votes']>=$QoutaAll)
+          					  {
+              						    $sql_insert = "INSERT INTO archive (position_name, winner_fname, winner_mname, winner_lname, school_year, platform_info) VALUES ('".$row1['position_name']."','".$row1['fname']."','".$row1['Mname']."','".$row1['lname']."','".$school_year."','".$row1['platform_info']."')";
+               						    $conn->query($sql_insert);
+           					   }
+        			  }
+			 else
+         			 {
+         				   $count_query1 = "SELECT * FROM student where grade_level = ".$row['grade_level']."";
+          				   $count_res1 = mysqli_query($conn, $count_query1);
+           				   $total1 = mysqli_num_rows($count_res1);
                                 
-            $QoutaAll1 = floor($total1/2)+1;
-            if($row['total_votes']>=$QoutaAll1)
-            {
-              $sql2 = "SELECT * from student where student_id= ".$row['student_id']."";
-              $q1 = $conn->query($sql2);
-              while($row1 = $q1->fetch_assoc())
-              {
-                $sql_insert = "INSERT INTO archive (position_name, winner_fname, winner_mname, winner_lname, school_year, platform_info) VALUES ('".$row['position_name']."','".$row1['fname']."','".$row1['Mname']."','".$row1['lname']."','".$school_year."','".$row['platform_info']."')";
-                $conn->query($sql_insert);
-              }
-            }
-          }
+           				   $QoutaAll1 = floor($total1/2)+1;
+           				  if($row1['total_votes']>=$QoutaAll1)
+           					 {
+              
+               						 $sql_insert = "INSERT INTO archive (position_name, winner_fname, winner_mname, winner_lname, school_year, platform_info) VALUES ('".$row1['position_name']."','".$row1['fname']."','".$row1['Mname']."','".$row1['lname']."','".$school_year."','".$row1['platform_info']."')";
+              						  $conn->query($sql_insert);
+             
+            					}
+         			 }
+			
+		}
+		
         }           
       }
 
