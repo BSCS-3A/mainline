5e3b820e2ce743a44fd90924ca59e6ffb39d3b3c
iveejntln <65966844+iveejntln@users.noreply.github.com>
2021-05-26 00:15:30 +0800

    Update reset.php

--- php/backMonitor/reset.php
+++ php/backMonitor/reset.php
@@ -2,6 +2,9 @@
     require '../db_conn.php';
 
     $truncate_record = mysqli_query($conn, 'TRUNCATE TABLE vote_event');
+    $truncate_vote = mysqli_query($conn, 'TRUNCATE TABLE vote');
+    $reset_voting_status = mysqli_query($conn, 'UPDATE student SET voting_status = 0');
+    $reset_total_votes = mysqli_query($conn, 'UPDATE candidate SET total_votes = 0');
 
     $file = fopen("../../other/post_result.txt", "w") or die("Unable to open file!");
 
