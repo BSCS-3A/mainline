1622c229246e475c76994172119a0a440b32f735
Steven Andrei Barrios <32772384+steven1034@users.noreply.github.com>
2021-06-01 02:52:31 +0800

    Update backFun_setcountdown.php
    may aayusin pa dito

--- php/backFun_setcountdown.php
+++ php/backFun_setcountdown.php
@@ -30,32 +30,49 @@ if (isset($_POST['editsched'])) {
     $endDate .= " $timeEnds";
 
 
-   if($startDate >= $endDate){
+   
+    if($startDate >= $endDate){
       function_alert("Incorrect Input"); 
     }else{
+
+      $date=$_POST['date'];
+      $dateEnd=$_POST['dateEnd'];
+      $tstart= $_POST['tstart'];
+      $tends= $_POST['tends'];
+      $indicator = $_POST['election'];
       
-    $truncate = mysqli_query($conn, "TRUNCATE TABLE vote_event"); 
-
-    $date=$_POST['date'];
-    $dateEnd=$_POST['dateEnd'];
-    $tstart= $_POST['tstart'];
-    $tends= $_POST['tends'];
-    $indicator = $_POST['election'];
-
-    //updating the table
-     $vtevent = "INSERT INTO vote_event (`start_date`,`end_date`,`vote_duration`) 
-      VALUES('$date $tstart', '$dateEnd $tends', '$indicator')";
-      mysqli_query($conn, $vtevent);
-        function_alert("Updating successful"); 
+        //$truncate = mysqli_query($conn, "TRUNCATE TABLE vote_event"); 
+        $event = mysqli_query($conn, "SELECT * FROM vote_event");
+        $row = mysqli_fetch_array($event);
     
+        if (empty($row)) {
+            $vtevent = "INSERT INTO vote_event (`start_date`,`end_date`) 
+                VALUES('$date $tstart', '$dateEnd $tends')";
+            mysqli_query($conn, $vtevent);
+            function_alert("Updating successful");
+         
+        }elseif(!empty($row)){
+            $query = "UPDATE `vote_event` SET start_date='$date $tstart', end_date='$dateEnd $tends' WHERE vote_event_id = 1";
+            mysqli_query($conn, $query);
+            function_alert("Updating successful"); 
+
+      }
       //For Logs
       $_SESSION['action'] = 'set Election Countdown.';
       include './backAdmin/backFun_actLogs_v0_1.php';
    }
+    
+    
+   if (isset($_POST['editperiod'])) {
+   $indicator = $_POST['election'];
 
- }else{
-   function_alert("Updating failed");
+   $query = "UPDATE `vote_event` SET vote_duration='$indicator' WHERE vote_event_id = 1";
+   mysqli_query($conn, $query);
+   function_alert("SAVED"); 
 
+   //For Logs
+   $_SESSION['action'] = 'set Election Period.';
+   include './backAdmin/backFun_actLogs_v0_1.php';
 }
 
    ?>
